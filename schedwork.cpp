eaeb29eaefb97f090eb32d1849dc96512a24322f
Jena Bhandari <jbhandar@usc.edu>
2023-04-07 08:14:51 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -18,12 +18,45 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const size_t day,
+    std::vector<size_t>& counts
+);
+
+// type for the ID of a worker
+typedef unsigned int Worker_T;
+
+// n-by-k Matrix of each of the k workers' availability over an n-day period
+typedef std::vector<std::vector<bool>> AvailabilityMatrix;
+
+// n-by-d matrix with the d worker IDs who are scheduled 
+// to work on each of the n days
+typedef std::vector<std::vector<Worker_T>> DailySchedule;
 
 
-// Add your implementation of schedule() and other helper functions here
-
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum 
+ *        shifts any single worker is allowed. Returns true if
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise. 
+ * You MAY define helper functions in schedworker.cpp
+* You MUST use a recursive approach that follows the general backtracking structure 
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over 
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -31,14 +64,78 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
+	if(avail.size() == 0U)
+	{
+		return false;
+  }
+  
+	sched.clear();
+  
 
+  sched.resize(avail.size());
+  std::vector<size_t> counts(avail[0].size(), 0);
 
-
+  // backtrackinggg
+  if ( backtrack(avail, dailyNeed, maxShifts, sched, 0, counts) ) 
+	{
+    return true;
+  } 
+	else 
+	{
+    return false;
+  }
 
 }
 
+
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const size_t day,
+    std::vector<size_t>& counts
+) 
+{
+  // all days scheduled!
+  if (day == avail.size()) 
+	{
+    return true;
+  }
+
+  // try all workers
+  for (size_t worker = 0; worker < avail[day].size(); ++worker) 
+	{
+    // is worker available?
+    if (avail[day][worker]) 
+		{
+      // exceed max shifts?
+      if (counts[worker] < maxShifts) 
+			{
+				// push the worker to the schedule
+        sched[day].push_back(worker);
+        counts[worker]++;
+
+        // if day is full, move to next day
+				if (sched[day].size() == dailyNeed) 
+				{
+					if (backtrack(avail, dailyNeed, maxShifts, sched, day+1, counts)) 
+					{
+						return true;
+					}
+					else
+					{
+						// backtrack if it don't worker
+            sched[day].pop_back();
+            counts[worker]--;
+					}
+				}
+
+                
+      }
+  	}
+	}
+
+    // can't schedule this day so backtrack!!
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <deque>
 #endif
 
 #include "wordle.h"
@@ -13,16 +14,120 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+std::string current, 
+int index, 
+const std::set<std::string>& dict, 
+std::set<std::string>& result, std::deque<char>& letters);
 
 // Definition of primary wordle function
+
+/**
+ * @brief Returns the list of all legal words in the provided dictionary
+ *        that meet the criteria provided in the `in` and `floating`
+ *        inputs.
+ * 
+ * @param [in] in - string of fixed, correct characters and `-`s to indicate
+ *                  the length of string and locations that must be filled in
+ * @param [in] floating  - Characters that must be used somewhere in the word
+ * @param [in] dict - Dictionary of strings of legal words
+ * @return std::set<std::string> - Set of all words that meet the criteria
+ */
+
+
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+
+
+    // Initialize the results set
+    std::set<std::string> results;
+
+		std::deque<char> letters;
+
+		//put in all blanks
+		for (char c: in)
+		{
+			if (c == '-')
+			{
+				letters.push_back('%');
+			}
+		}
+
+		//for each floating letter, delete a blank and put in the letter
+		for (char b: floating)
+		{
+			letters.pop_back();
+			letters.push_front(b);
+		}
+
+
+		//make copy of in
+		std::string current = in;
+		wordleHelper(current, 0, dict, results, letters);
+
+
+    return results;
 
 }
 
+
 // Define any helper functions here
+void wordleHelper(
+std::string current, 
+int index, 
+const std::set<std::string>& dict, 
+std::set<std::string>& result, 
+std::deque<char>& letters) 
+{
+		//if to length and all floating letters within then check if in dict
+    if (index == current.length() && letters.empty()) 
+		{
+        if (dict.find(current) != dict.end()) 
+				{
+          result.insert(current);
+        }
+        return;
+    }
+
+		//if fixed then recurse
+    if (current[index] != '-') 
+		{
+        wordleHelper(current, index + 1, dict, result, letters);
+    } 
+		else 
+		{
+			//go thru the deck
+			for (int i = 0; i < letters.size(); i++)
+			{
+				char frontLetter = letters.front();
+
+				if (frontLetter == '%')
+				{
+					letters.pop_front();
+					for (char c = 'a'; c <= 'z'; c++) 
+					{
+        		current[index] = c;
+        		wordleHelper(current, index + 1, dict, result, letters);
+						current[index] = '-';
+    			}
+
+					letters.push_back('%');
+				}
+				else 
+				{
+					letters.pop_front();
+
+					current[index] = frontLetter;
+					wordleHelper(current, index + 1, dict, result, letters);
+					current[index] = '-';
+
+					letters.push_back(frontLetter);
+				}
+    		
+			}
+
+    }
+}
